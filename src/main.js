c184627cca8e140b4a331f2874aa87b017d9f22f
linyu-ch <linyuenhong@gmail.com>
2024-12-26 16:56:25 -0800

    initial commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { StreamerbotClient } from "@streamerbot/client";
+
+// initiating connection with streamerbot: see https://streamerbot.github.io/client/guide/events
+const client = new StreamerbotClient();
+
+
+
