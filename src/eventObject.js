1da63ca5de36011d50771bf6cd814cfbf35bcdbf
linyu-ch <linyuenhong@gmail.com>
2024-12-27 00:18:45 -0800

    updated eventObject definition to include senderId

--- src/eventObject.js
+++ src/eventObject.js
@@ -1,6 +1,7 @@
 export class EventObj {
     constructor(obj) {
         this.eventId = obj.eventId; // string
+        this.senderId = obj.senderId; // string
         this.timestamp = obj.timestamp; // date
         this.eventType = obj.eventType; // string
         this.senderName = obj.senderName; // string
