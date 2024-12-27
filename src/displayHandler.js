2efd069ca2094b02c1ff07ebf61d11bc3e95c5a5
linyu-ch <linyuenhong@gmail.com>
2024-12-27 00:19:44 -0800

    implemented push, pop and deletion operations in display handler, added new optional max count parameter to constructor

--- src/displayHandler.js
+++ src/displayHandler.js
@@ -1,14 +1,52 @@
 export class DisplayHandler {
-    constructor(display) {
+  constructor(display, maxCount = 30) {
     this.value = 0;
     this.display = display;
+    this.maxCount = maxCount;
   }
 
   pushToDisplay(EventObj) {
+    let row = document.createElement("div");
+    row.dataset.eventId = EventObj.eventId;
+    row.dataset.senderId = EventObj.senderId;
+    row.dataset.timestamp = EventObj.timestamp;
+    row.dataset.eventType = EventObj.eventType;
+    row.dataset.senderName = EventObj.senderName;
+
+    row.innerHTML = EventObj.payload;
+
+    this.display.prepend(row);
     this.value++;
+
+    if (this.value > this.maxCount) {
+        this.popFromDisplay();
+    }
+
   }
 
-  removeFromDisplay(identifier){
+  popFromDisplay() {
+    if (this.value === 0) {
+      return;
+    }
+
+    this.display.lastChild.remove();
+    this.value--;
+  }
+
+  removeFromDisplay(identifier) {
     // loop through each item in the display, and remove the items that match the identifier
+    this.display.children.forEach((child) => {
+      if (
+        child.dataset.eventId === identifier ||
+        child.dataset.senderId === identifier ||
+        child.dataset.timestamp === identifier ||
+        child.dataset.eventType === identifier ||
+        child.dataset.senderName === identifier
+      ) {
+        // TODO: add more fancy removal effects in next version
+        child.remove();
+        this.value--;
+      }
+    });
   }
 }
