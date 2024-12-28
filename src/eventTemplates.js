94a294924d03e8bdd940a23a21d5a8b4ed2376c5
linyu-ch <linyuenhong@gmail.com>
2024-12-27 22:32:33 -0800

    set up event templates

--- src/eventTemplates.js
+++ src/eventTemplates.js
@@ -1,26 +1,83 @@
-// Todo: implement the templates
+export const messageEvent = (username, message, roleTierAttribute = "none", platform)=>{
+    let payload = `<div class="chat__wrapper" data-role="${roleTierAttribute}">
+      <div class="chat__username">
+        <p class="chat__platform-identifier">${platform}</p>
+        <p class="chat__username-text">${username}</p>
+      </div>
+      <div class="chat__content">
+          <p class="chat__content-message">
+            ${message}
+          </p>
+      </div>
+    </div>`
 
+    return payload;
+}
+export const subscriptionEvent = (username, platformNoun)=>{
+    let payload = `<div class="event__wrapper">
+    <div class="event__body">
+            <div class="event__username">${username}</div>
+            <div class="event__content--action">${platformNoun}</div>
+        </div>
+  </div>`
+  
+  return payload;
+}
+export const paidSubscriptionEvent = (username, giftString)=>{
+    let payload = `<div class="event__wrapper">
+    <div class="event__body">
+            <div class="event__username">${username}</div>
+            <div class="event__content--action">gifted ${giftString}</div>
+        </div>
+  </div>`
 
-export const messageEvent = (username, message, platform)=>{
-    throw new Error('This template has not yet been implemented');    
-}
-export const subscriptionEvent = (username, platform)=>{
-    throw new Error('This template has not yet been implemented');    
-}
-export const paidSubscriptionEvent = (username, amount, platform)=>{
-    throw new Error('This template has not yet been implemented');    
-}
-export const donationEvent = (username, amount, currency)=>{
-    throw new Error('This template has not yet been implemented');    
-}
-export const giftEvent = (username, amount, platform)=>{
-    throw new Error('This template has not yet been implemented');    
-}
-export const massGiftEvent = (username, amount, platform)=>{
-    throw new Error('This template has not yet been implemented');    
+  return payload;
 }
 
+/* external donation events are for 3rd party platforms that conduct in direct currencies
+  which don't have inbuilt platform specific attributes
+*/
+export const externalDonationEvent = (username, donationString)=>{
+    let payload = `<div class="event__wrapper">
+    <div class="event__body">
+            <div class="event__username">${username}</div>
+            <div class="event__content--action">${donationString}</div>
+        </div>
+  </div>`
 
-// these should be handled separately
-export const messageDeleteEvent = ()=>{}
-export const userDeleteEvent = ()=>{}
+  return payload;
+}
+
+/* external donation events are for platform centered operations which usually include tiers
+colors, and various currencies
+*/
+export const platformDonationEvent = (username, donationString, eventTierAttribute = none)=>{
+    let payload = `<div class="event__wrapper" data-tier="${eventTierAttribute}">
+    <div class="event__body">
+            <div class="event__username">${username}</div>
+            <div class="event__content--action">${donationString}</div>
+        </div>
+  </div>`
+  
+  return payload;
+}
+export const giftEvent = (username, giftString)=>{
+    let payload = `<div class="event__wrapper">
+    <div class="event__body">
+            <div class="event__username">${username}</div>
+            <div class="event__content--action">${giftString}</div>
+        </div>
+  </div>`
+
+  return payload;
+}
+export const massGiftEvent = (username, giftString)=>{
+    let payload = `<div class="event__wrapper">
+    <div class="event__body">
+            <div class="event__username">${username}</div>
+            <div class="event__content--action">${giftString}</div>
+        </div>
+  </div>`
+
+  return payload;
+}
