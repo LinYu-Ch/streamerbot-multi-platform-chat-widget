3cf802c82d974ba1a0bb1a226dddeabad2f13f2e
linyu-ch <linyuenhong@gmail.com>
2024-12-29 01:57:55 -0800

    re structured the event object order

--- src/eventObject.js
+++ src/eventObject.js
@@ -1,9 +1,9 @@
 export class EventObj {
     constructor(obj) {
+        this.timestamp = obj.timestamp; // date
         this.eventId = obj.eventId; // string
         this.senderId = obj.senderId; // string
-        this.timestamp = obj.timestamp; // date
-        this.eventType = obj.eventType; // string
+        this.eventInfo = obj.eventInfo; // string
         this.senderName = obj.senderName; // string
         this.payload = obj.payload; // HTML string literal
     }
