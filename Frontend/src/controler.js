83f47a6617eef6822adfbf7f6f34de3f671ee47d
linyu-ch <linyuenhong@gmail.com>
2025-03-25 19:39:52 -0500

    moved emote handler logic to controler, and organized code

--- Frontend/src/controler.js
+++ Frontend/src/controler.js
@@ -9,9 +9,9 @@ import { io } from "socket.io-client";
 const PORT = 10890;
 const socket = io(`http://localhost:${PORT}`);
 
-function sendToServer(payload, socket) {
+function sendToServer(payload, socket, channel) {
   console.log("sending:", payload);
-  socket.emit("control", payload);
+  socket.emit(channel, payload);
 }
 
 import {
@@ -52,6 +52,7 @@ const eventMappings = [
 ];
 
 socket.on("connect", () => {
+
   console.log("(SBCHAT) CONTROLER connection with backend server established on port: ", PORT);
   eventMappings.forEach(({ id, generator, handler }) => {
     const btn = document.getElementById(id);
@@ -62,10 +63,66 @@ socket.on("connect", () => {
           eventType: handler,
           eventData: eventData,
         }
-        sendToServer(eventPayload, socket);
+        sendToServer(eventPayload, socket, "control");
       });
     }
   });
 
+  const youtubeEmoteForm = document.getElementById('youtube-emote-inputs');
+  const youtubeEmoteFormSubmit = document.getElementById('save-emotes');
+
+  youtubeEmoteFormSubmit.addEventListener('click', () => {
+    const formData = new FormData(youtubeEmoteForm);
+    const formPayload = {};
+
+    // separating form data into an array of values so its easier to throw into the payloads
+    let tempNameValue = "";
+    console.log(Array.from(formData.values()));
+    Array.from(formData.values()).forEach((value, index) => {
+      // if index isn't even, if its a name
+      if (index % 2 === 0) {
+        tempNameValue = value;
+      } else {
+        formPayload[tempNameValue] = {
+          data: value,
+          type: value.type,
+        }
+      }
+    });
+
+    sendToServer(formPayload, socket, "emotes");
+  })
+
+  
+  const emoteBlockFactory = (emoteInputBlock, label = null, image = null) => {
+    const newEmoteBlock = emoteInputBlock.cloneNode(true);
+    emoteForm.prepend(newEmoteBlock);
+
+    const fileInput = newEmoteBlock.querySelector('.upload-emote');
+    const emoteLabel = newEmoteBlock.querySelector('.label-emote');
+    const emotePreview = newEmoteBlock.querySelector('.emote-preview');
+    const deleteBlock = newEmoteBlock.querySelector('.remove-emote');
+
+    // detects change on file input, then sets the emote preview to the image
+    // reference: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file
+    fileInput.addEventListener('change', () => {
+      const curFiles = fileInput.files;
+      if (curFiles.length === 0) { console.error('File input unsuccessful'); return }
+      emotePreview.src = URL.createObjectURL(curFiles[0]);
+    });
+
+    deleteBlock.addEventListener('click', () => {
+      newEmoteBlock.remove();
+    });
+  }
+  // logic to create and manage the state of add youtube emote blocks
+  const emoteForm = document.getElementById('youtube-emote-inputs');
+  const emoteInputBlock = document.getElementById('emote-input-block');
+  const addEmote = document.getElementById('add-emote');
+
+  // adds a new emote block and attatches event listeners to them
+  addEmote.addEventListener("click", () => {
+    emoteBlockFactory(emoteInputBlock);
+  });
 });
 
